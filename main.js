4d76781963399ef5075161ee34524ad28ebc41bb
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-04 12:28:48 -0300

    arregle la funcion

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 let form = document.getElementById("loginForm");
 
-/*import validator from "validator";*/
+function validar (e){
 
-function validar (e) {
+    e.preventDefault();
 
-    e.preventDeFault ();
-    
-    if (!validator.isAlphanumeric (form.Card.value) || validator.isEmpty ( form.Card.value)) { 
+    if (!validator.isCreditCard (form.Card.value) || validator.isEmpty ( form.Card.value)) { 
         form.Card.focus ();
         form.Card.classList.add("bg-danger");
         form.Card.classList.remove("is-valid");
@@ -15,8 +13,8 @@ function validar (e) {
         form.Card.classList.add("is-valid");
     }
 
-    if (!validator.isAlphanumeric (form.CVC.value) || validator.isEmpty ( form.CVC.value)) { 
-        form.Card.focus ();
+    if (!validator.isEmpty ( form.CVC.value)) { 
+        form.CVC.focus ();
         form.CVC.classList.add("bg-danger");
         form.CVC.classList.remove("is-valid");
     } else {
@@ -24,63 +22,33 @@ function validar (e) {
         form.CVC.classList.add("is-valid");
     }
 
-
-    }
-
-    if (!form.Amount.value.trim().length > 0){
-        enviar = false;
+    if (!validator.isNumeric (form.Amount.value) || validator.isEmpty (form.Amount.value)) { 
         form.Amount.focus ();
         form.Amount.classList.add("bg-danger");
+        form.Amount.classList.remove("is-valid");
     } else {
         form.Amount.classList.remove("bg-danger");
         form.Amount.classList.add("is-valid");
     }
-    
-    if (!form.firstName.value.trim().length > 0){
-        enviar = false;
+   
+    if (!validator.isAlpha (form.firstName.value) || validator.isEmpty (form.firstName.value)) { 
         form.firstName.focus ();
         form.firstName.classList.add("bg-danger");
+        form.firstName.classList.remove("is-valid");
     } else {
         form.firstName.classList.remove("bg-danger");
         form.firstName.classList.add("is-valid");
     }
 
-    if (!form.lastName.value.trim().length > 0){
-        enviar = false;
+    if (!validator.isAlpha (form.lastName.value) || validator.isEmpty (form.lastName.value)) { 
         form.lastName.focus ();
         form.lastName.classList.add("bg-danger");
+        form.lastName.classList.remove("is-valid");
     } else {
         form.lastName.classList.remove("bg-danger");
         form.lastName.classList.add("is-valid");
     }
 
-    if (!form.City.value.trim().length > 0){
-        enviar = false;
-        form.City.focus ();
-        form.City.classList.add("bg-danger");
-    } else {
-        form.City.classList.remove("bg-danger");
-        form.City.classList.add("is-valid");
-    }
-
-    if (!form.inputState.value.trim().length > 0 ){
-        enviar = false;
-        form.inputState.focus ();
-        form.inputState.classList.add("bg-danger");
-    } else {
-        form.inputState.classList.remove("bg-danger");
-        form.inputState.classList.add("is-valid");
-    }
-
-    if (!form.postalCode.value.trim().length > 0){
-        enviar = false;
-        form.postalCode.focus ();
-        form.postalCode.classList.add("bg-danger");
-    } else {
-        form.postalCode.classList.remove("bg-danger");
-        form.postalCode.classList.add("is-valid");
-    }
-    
-
-    return enviar;
 }
+
+form.addEventListener("submit", validar);
